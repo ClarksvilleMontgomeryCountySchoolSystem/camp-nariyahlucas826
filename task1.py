6b44cf92520523ca606e650e39e072f957ee4f30
nariyahlucas826 <nariyah.lucas826@students.cmcss.net>
2026-02-24 10:25:15 -0600

    N

--- task1.py
+++ task1.py
@@ -1,37 +1,44 @@
 def main():
     #Collect Input for each
     #Variable: first Prompt: Child's First Name:
-    
+    first = input("Child's First Name: ")
     #Variable: last Prompt: Child's Last Name:
-    
+    last = input("Child's last Name: ")
+
     print(f"Camper's Name: {first} {last}")
 
     #Variable: birth Prompt: In what year was {first} {last} born:
-    
+    birth = input(f"In what year was {first} {last} born: ")
+
     print(f"Birth Year: {birth}")
 
     #Variable: days Prompt: How many days will {first} attend?
-    
+    days = input(f"How many days will {first} attend? ")
+
     print(f"Camp Duration: {days} days")
 
     #Variable: p_first Prompt: Parent's First Name:
     
     #Variable: p_last Prompt: Parent's Last Name:
     
-    print(f"Parent's Name: {p_first} {p_last}")
+    print(f"Parent's Name: {first} {last}")
 
     #Variable: phone Prompt: Parent's Phone #:
     
-    print(f"Phone Number: {phone}")
+    print(f"Phone Number: (phone)")
 
     #Variable: street Prompt: Street Address:
-    
+    print(f"Street Address: ")\
+
     #Variable: city Prompt: City:
-    
+    print(f"City: ")
+
     #Variable: state Prompt: State Abbreviation:
-    
+    print(f"State Abbreviation: ")
+
     #Variable: zip Prompt: Zip Code:
-    
+    print(f"Zip Code: ")
+
     print(f"Address:\n{street}\n{city}, {state} {zip}")
 
 if __name__ == "__main__":
